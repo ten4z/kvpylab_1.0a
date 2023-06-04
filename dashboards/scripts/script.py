b6fa753970a478095b45fcf408ab20c48d2ffa90
Josiel Soares <115186280+ten4z@users.noreply.github.com>
2023-06-04 13:23:35 -0300

    Add files via upload

--- dashboards/scripts/script.py
+++ dashboards/scripts/script.py
@@ -3,46 +3,40 @@ from random import choice, randint
 cont = bge.logic.getCurrentController()
 own = cont.owner
 scn = own.scene
+scl = bge.logic.getSceneList()
 obj = scn.objects
 class Game():
 	lista = []
-	pontos = 0
+	tempo = 1.3
 	def atualizar_dashboard(self):
-		if own['tmp2'] > 2 and own['tmp2'] <= 2.05:
-			obj['dash_c']['indicador'] = randint(0, 100)
+		if own['tmp2'] > self.tempo and own['tmp2'] <= self.tempo + 0.05:
+			
 			self.graficos_s()
-		if own['tmp2'] <= 2:
+		if own['tmp2'] <= self.tempo:
 			pass
 		else:
 			own['tmp2']	 = 0
+	
 	def graficos_s(self):
 		ep = obj['Empty_Plotador']
 		eo = obj['Empty_Origem']
 		#if cont.sensors['tap'].positive:
-		lista = [[0,0]]
-		
-		
-		
-		
-		self.pontos += 1
-		lista.pop(0)
+		for sc in scl:
+			if sc.name == 'back_circle':
+				sc.objects['dash_c']['indicador'] = randint(0, 100)
+		self.lista = [[0,0]]
+		self.lista.pop(0)
 		px = randint(0,5)
 		py = randint(0,5)
-		lista.append([px, py])
-		ep.localPosition = [4.05 +lista[0][0]*1.5, 2.05544, 7.01727 +  1.5*lista[0][1]]
+		self.lista.append([px, py])
+		ep.localPosition = [4 +self.lista[0][0]*1.5, 2.05544, 7.01727 +  1.5*self.lista[0][1]]
 		v0 = scn.addObject('v_inicio', ep)
-		own['pontos'] += 1
-		v0['id'] = 5 - own['pontos']
+		
 		v0.setParent(eo)
-		eo.localPosition.x -= 1.05
-		if v0['id'] <= 0:
-			own['pontos'] = 5
-			v0.endObject()
-
-		print(v0['id'])
+		eo.localPosition.x -= 1.0
+		#print(v0['id'])
 		#v0.endObject()
-		#v1['id'] -= 1
-		print(lista)
+		print(self.lista)
 		'''
 		own.localPosition.x  = 0
 		own.localPosition.z  = a*0+b
@@ -59,13 +53,13 @@ class Game():
 		r.alignAxisToVect(direction, AXIS_X)
 		'''
 		#v1 = scn.addObject('v_inicio', )
-	def iniciar(self):
+
+	def main_sc(self):
 		#print('Game iniciando...')	
 		if own['tmp1'] >= 0 and own['tmp1'] < 0.75:
 			obj['txt_titulo']['Text'] = 1
 		elif own['tmp1'] >= 0.75 and own['tmp1'] <= 1.5:
 			obj['txt_titulo']['Text'] = 0	
-
 		else:
 			txt = choice(['sistemas', 'cursos','conteudo','multimedia','tecnologia','produtos', 'servicos'])
 			if txt not in self.lista:
@@ -78,12 +72,16 @@ class Game():
 				self.lista.insert(0, 'multimedia')							
 							
 			own['tmp1'] = 0	
-		'''
+		
 		print(len(self.lista))
 		print(self.lista)			
 		#print(own['tmp1'])
-		'''
+		
+		
 def run():
-	g = Game()
-	g.iniciar()
+	g = Game()	
+	
 	g.atualizar_dashboard()
+	g.main_sc()
+	
+	
